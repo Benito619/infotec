2046e012e9b0ba49c90612e679b2e2e644d25891
Benito619 <ari_y_sar@hotmail.com>
2025-12-16 23:01:04 +0000

    Cambios respecto al archivo api.php

--- src/routes/api.php
+++ src/routes/api.php
@@ -5,6 +5,20 @@ use Illuminate\Support\Facades\Route;
 // Importar el controlador EventoController
 use App\Http\Controllers\EventoController;
 
+/**
+* Rutas para el recurso Evento.
+*/
+// Recuperar todos los eventos
+Route::get('/eventos', [EventoController::class, 'index']);
+// Almacenar un evento nuevo
+Route::post('/eventos', [EventoController::class, 'store']);
+// Recuperar un evento específico
+Route::get('/eventos/{id}', [EventoController::class, 'show']);
+// Actualizar un evento específico
+Route::put('/eventos/{evento}', [EventoController::class, 'update']);
+// Eliminar un evento específico
+Route::delete('/eventos/{id}', [EventoController::class, 'destroy']);
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
